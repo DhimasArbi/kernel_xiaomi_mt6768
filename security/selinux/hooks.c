c34a9d6f1de6db69c30b6a1fac894679ca62aa1c
DhimasArbi <dhimas.arbi@gmail.com>
2022-01-13 11:31:05 +0000

    fix conflict hooks.c

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2240,16 +2240,11 @@ static int selinux_binder_transaction(const struct cred *from,
 static int selinux_binder_transfer_binder(const struct cred *from,
 					  const struct cred *to)
 {
-<<<<<<< HEAD
 	u32 fromsid = task_sid(from);
 	u32 tosid = task_sid(to);
 
 	return avc_has_perm(&selinux_state,
 			    fromsid, tosid, SECCLASS_BINDER, BINDER__TRANSFER,
-=======
-	return avc_has_perm(cred_sid(from), cred_sid(to),
-			    SECCLASS_BINDER, BINDER__TRANSFER,
->>>>>>> 253cad9a9560b39321c5c8ec3343838a01b3c787
 			    NULL);
 }
 
